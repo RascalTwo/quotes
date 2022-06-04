c59e577aeda2d1b69fba2b15c6fcef6ce343fd45
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-04 16:56:39 -0400

    Add MongoDB Client

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import 'dotenv/config';
+import { MongoClient } from 'mongodb'
 import express from 'express';
 
 
 const PORT = process.env.PORT || 1337;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
 
@@ -14,4 +16,12 @@ app.get('/secret', (request, response) => {
 	response.send(process.env.SECRET);
 });
 
-app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+
+if (!MONGODB_URL) throw new Error('MONGODB_URL environment variable required.')
+
+const client = new MongoClient(process.env.MONGODB_URL);
+client.connect().then(client => {
+  console.log('Connected successfully to server');
+
+	app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+}).catch(console.error)
