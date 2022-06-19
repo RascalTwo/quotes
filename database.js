71461a4d80b6b67d32c90807ac00ea8be24b5bca
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-19 02:55:58 -0400

    Fix tests being blocked by missing environment variable

--- database.js
+++ database.js
@@ -2,9 +2,10 @@ import { MongoClient }  from 'mongodb';
 
 /** @type {MongoClient} */
 let client = null;
-/* c8 ignore next */
-if (!process.env.MONGODB_URL) throw new Error('MONGODB_URL environment variable required.')
 export default () =>{
+	/* c8 ignore next */
+	if (!process.env.MONGODB_URL) throw new Error('MONGODB_URL environment variable required.')
+
 	if (!client) client = new MongoClient(process.env.MONGODB_URL);
 	return client;
 }
