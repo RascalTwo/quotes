c4c734922e8609b725130b2e7d263420caf370ed
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-25 17:24:35 -0400

    Shorten next & previous button code

--- public/index.js
+++ public/index.js
@@ -20,7 +20,7 @@ new class ShowAutocompletes {
 		}, document.createDocumentFragment()));
 	}
 
-	populateShowInfo(){
+	populateShowInfo() {
 		return fetch(`/api/show-info?show=${encodeURIComponent(this.showName)}`)
 			.then(r => r.json())
 			.then(info => this.shows[this.showName] = info)
@@ -42,6 +42,7 @@ new class ShowAutocompletes {
 	}
 }(showInput, seasonInput, seasonList, episodesList);
 
+const IDENTIFIER_KEYS = ['show', 'season', 'episodes', 'timeStamp'];
 
 function handleRelativeQuoteClick(relative, { currentTarget: button }) {
 	button.disabled = true;
@@ -51,14 +52,11 @@ function handleRelativeQuoteClick(relative, { currentTarget: button }) {
 		show: li.dataset.show,
 		season: +li.dataset.season,
 		episodes: JSON.parse(li.dataset.episodes),
-		timeStamp: +li.dataset.timestamp,
-		text: li.dataset.text,
+		timeStamp: +li.dataset.timestamp
 	}
 	const url = new URL('/api/' + relative, window.location.origin);
-	url.searchParams.set('show', quote.show);
-	url.searchParams.set('season', quote.season);
-	url.searchParams.set('episodes', quote.episodes);
-	url.searchParams.set('timeStamp', quote.timeStamp);
+	for (const key of IDENTIFIER_KEYS) url.searchParams.set(key, quote[key]);
+
 	return fetch(url.toString()).then(r => r.json()).then(({ quote }) => {
 		if (quote === null) return
 
@@ -71,9 +69,8 @@ function handleRelativeQuoteClick(relative, { currentTarget: button }) {
 		newLI.className = 'quote-item'
 		newLI.dataset.show = quote.show;
 		newLI.dataset.season = quote.season;
-		newLI.dataset.episodes = JSON.stringify(quote.episodes);
 		newLI.dataset.timestamp = quote.timeStamp;
-		newLI.dataset.text = quote.text;
+		newLI.dataset.episodes = JSON.stringify(quote.episodes);
 
 		newLI.innerHTML = `
 			<button class="previous-button" aria-label="Show Previous Quote" ${relative === 'next' ? 'disabled' : ''} ><</button>
@@ -83,12 +80,14 @@ function handleRelativeQuoteClick(relative, { currentTarget: button }) {
 			<button class="next-button" aria-label="Show Next Quote" ${relative === 'previous' ? 'disabled' : ''}>></button>
 			<div class="text">${quote.text.replace(new RegExp('(' + [...document.querySelector('#original-query').value].map(char => `[${char}]`).join('') + ')', 'ig'), '<b>$1</b>')}</div>
 		`;
-		newLI.querySelector('.previous-button').addEventListener('click', handleRelativeQuoteClick.bind(null, 'previous'));
-		newLI.querySelector('.next-button').addEventListener('click', handleRelativeQuoteClick.bind(null, 'next'));
-		if (relative === 'previous') li.parentElement.insertBefore(newLI, li);
-		else li.parentElement.insertBefore(newLI, li.nextElementSibling);
+		newLI.querySelectorAll('button').forEach(button =>
+			button.addEventListener('click', handleRelativeQuoteClick.bind(null, button.className.split('-')[0]))
+		);
+
+		li.parentElement.insertBefore(newLI, relative === 'previous' ? li : li.nextElementSibling);
 	});
 }
 
-document.querySelectorAll('.previous-button').forEach(button => button.addEventListener('click', handleRelativeQuoteClick.bind(null, 'previous')));
-document.querySelectorAll('.next-button').forEach(button => button.addEventListener('click', handleRelativeQuoteClick.bind(null, 'next')));
+document.querySelectorAll('ul button').forEach(button =>
+	button.addEventListener('click', handleRelativeQuoteClick.bind(null, button.className.split('-')[0]))
+);
