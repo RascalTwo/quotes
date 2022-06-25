74c5f14665fd9ab029b801de9066cf5d33613a07
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-25 17:18:01 -0400

    Add previous and next quote buttons

--- public/index.js
+++ public/index.js
@@ -41,3 +41,54 @@ new class ShowAutocompletes {
 		if (season) this.renderDatalist(this.episodesList, this.shows[this.showName]?.[season] || []);
 	}
 }(showInput, seasonInput, seasonList, episodesList);
+
+
+function handleRelativeQuoteClick(relative, { currentTarget: button }) {
+	button.disabled = true;
+
+	const li = button.parentElement;
+	const quote = {
+		show: li.dataset.show,
+		season: +li.dataset.season,
+		episodes: JSON.parse(li.dataset.episodes),
+		timeStamp: +li.dataset.timestamp,
+		text: li.dataset.text,
+	}
+	const url = new URL('/api/' + relative, window.location.origin);
+	url.searchParams.set('show', quote.show);
+	url.searchParams.set('season', quote.season);
+	url.searchParams.set('episodes', quote.episodes);
+	url.searchParams.set('timeStamp', quote.timeStamp);
+	return fetch(url.toString()).then(r => r.json()).then(({ quote }) => {
+		if (quote === null) return
+
+		const alreadyExists = document.querySelector(`[data-show="${quote.show}"][data-season="${quote.season}"][data-episodes="${JSON.stringify(quote.episodes)}"][data-timestamp="${quote.timeStamp}"]`);
+		if (alreadyExists) {
+			return alreadyExists.querySelector(`.${relative === 'previous' ? 'next' : 'previous'}-button`).disabled = true;
+		}
+
+		const newLI = document.createElement('li')
+		newLI.className = 'quote-item'
+		newLI.dataset.show = quote.show;
+		newLI.dataset.season = quote.season;
+		newLI.dataset.episodes = JSON.stringify(quote.episodes);
+		newLI.dataset.timestamp = quote.timeStamp;
+		newLI.dataset.text = quote.text;
+
+		newLI.innerHTML = `
+			<button class="previous-button" aria-label="Show Previous Quote" ${relative === 'next' ? 'disabled' : ''} ><</button>
+			<code class="identifier">
+				S${quote.season} E${quote.episodes.join('-')} at ${new Date(quote.timeStamp * 1000).toISOString().substr(11, 8)}
+			</code>
+			<button class="next-button" aria-label="Show Next Quote" ${relative === 'previous' ? 'disabled' : ''}>></button>
+			<div class="text">${quote.text.replace(new RegExp('(' + [...document.querySelector('#original-query').value].map(char => `[${char}]`).join('') + ')', 'ig'), '<b>$1</b>')}</div>
+		`;
+		newLI.querySelector('.previous-button').addEventListener('click', handleRelativeQuoteClick.bind(null, 'previous'));
+		newLI.querySelector('.next-button').addEventListener('click', handleRelativeQuoteClick.bind(null, 'next'));
+		if (relative === 'previous') li.parentElement.insertBefore(newLI, li);
+		else li.parentElement.insertBefore(newLI, li.nextElementSibling);
+	});
+}
+
+document.querySelectorAll('.previous-button').forEach(button => button.addEventListener('click', handleRelativeQuoteClick.bind(null, 'previous')));
+document.querySelectorAll('.next-button').forEach(button => button.addEventListener('click', handleRelativeQuoteClick.bind(null, 'next')));
